ad76a3b5554d20b92d6fac037d6c40f3619eea3b
Jeremie <jeremie@torop.net>
2014-10-17 18:02:28 +0200

    Issue Status return a Status object

--- src/JiraRestClient/Issue.php
+++ src/JiraRestClient/Issue.php
@@ -218,7 +218,7 @@ class Issue
     public function getStatus()
     {
         if (isset($this->fields['Status'])) {
-            return $this->fields['Status'];
+            return new Status($this->fields['Status']);
         }
     }
 
